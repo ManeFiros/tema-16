af857e2cd15c5e832c782180c8eef4b00e86630b
Mané <sendmyasith@gmail.com>
2021-05-22 16:56:16 +0200

    useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,28 @@
 import logo from './logo.svg';
 import './App.scss';
-import React, {useReducer} from 'react';
+import React, {useReducer, useState} from 'react';
 import {BrowserRouter as Router, Route, Redirect} from 'react-router-dom';
-import Nav from './components/Nav/Nav.component';
+//import Nav from './components/Nav/Nav.component';
 //import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute.component';
 //import Login from './pages/Login/Login.page';
 import About from './pages/About/About.page';
 //import Store from './pages/Store/Store.page';
 //import Product from './pages/Product/Product.page';
 import Context from './context';
-import { reducerIsLogged, initialStateIsLogged, LOG_IN, LOG_OUT, DIA, NOCHE, reducerIsDeDia, initialStateIsDeDia} from './context/reducers';
+//import { reducerIsLogged, initialStateIsLogged, LOG_IN, LOG_OUT, DIA, NOCHE, reducerIsDeDia, initialStateIsDeDia} from './context/reducers';
 
 function App() {
 
-  const [isLogged, dispatch] = useReducer(reducerIsLogged, initialStateIsLogged);
-  const [isDeDia, dispatchMode] = useReducer(reducerIsDeDia, initialStateIsDeDia);
+  const [isLogged, dispatch] = useState(true);//useReducer(reducerIsLogged, initialStateIsLogged);
+  const [isDeDia, dispatchMode] = useState(true);//useReducer(reducerIsDeDia, initialStateIsDeDia);
 
   let logOut = () => { /*dispatch({type: LOG_OUT});*/ }
 
   let logIn = () => { /*dispatch({type: LOG_IN});*/ }
 
-  let hazDia = () => {/*dispatchMode({type: DIA});*/ }
+  let hazDia = () => {/* dispatchMode({type: DIA});*/ }
 
-  let hazNoche = () => {/* dispatchMode({type: NOCHE});*/ }
+  let hazNoche = () => { /*dispatchMode({type: NOCHE});*/ }
 
   return (
     <div className="App">
