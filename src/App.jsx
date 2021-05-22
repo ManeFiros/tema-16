ac404d11634d503d8488b61d900fb44d3e399485
Mané <sendmyasith@gmail.com>
2021-05-22 17:25:33 +0200

    login

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import logo from './logo.svg';
 import './App.scss';
-import React, {useReducer, useState} from 'react';
+import React, {useReducer} from 'react';
 import {BrowserRouter as Router, Route, Redirect} from 'react-router-dom';
 //import Nav from './components/Nav/Nav.component';
 //import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute.component';
-//import Login from './pages/Login/Login.page';
+import Login from './pages/Login/Login.page';
 import About from './pages/About/About.page';
 //import Store from './pages/Store/Store.page';
 //import Product from './pages/Product/Product.page';
@@ -41,7 +41,7 @@ function App() {
         <div className={isDeDia?"Dia":"Noche"}>
           <Router>
             {/*<Nav/>*/}
-            <Route exact path="/" component={About}></Route>
+            <Route exact path="/" component={Login}></Route>
             {/*<ProtectedRoute exact path="/store" component={Store} />
             <ProtectedRoute path="/product/:MODIFICABLE" component={Product}/>
     <ProtectedRoute exact path="/about" component={About}/>*/}
